29f75fd14db09d1f934500080a58eedd38d7444b
mark mersenski <MJMERSENSKI@QG.com>
2018-01-27 11:39:21 -0600

    created default ASP.NET website project

---
+++ JavaCycles/JavaCycles/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JavaCycles.Startup))]
+namespace JavaCycles
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
